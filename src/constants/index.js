fa088c116aa861dc205cb90d665c4e78e84fa42a
Nitin <157195670+ethicalnitin@users.noreply.github.com>
2025-02-21 01:07:44 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
 import project1 from "../assets/projects/project1.png";
 import project2 from "../assets/projects/LeloYar.png";
 
-export const HERO_CONTENT = 'A full-stack developer who is good at DSA and an AI enthusiast, turning challenges into innovative solutions';
+export const HERO_CONTENT = 'The one who gets the job done.';
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full-stack developer with a passion for creating efficient and user-friendly web applications. I have worked on different techs like JavaScript, HTML, CSS, C++, Python, React.js, Node.js, Express.js, and state management with Redux Toolkit and Context API. I’m adept at API development with REST and GraphQL and skilled in database management with MongoDB, PostgreSQL, and MySQL. I thrive in collaborative environments, solving complex problems, and delivering high-quality solutions. Outside of coding, I enjoy exploring new technologies and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full-stack developer with a passion for creating efficient and user-friendly web applications. I have worked on different techs like JavaScript, HTML, CSS, C++, Python, React.js, Node.js, Express.js, and state management with Redux Toolkit and Context API.. Outside of coding, I enjoy exploring new technologies and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
@@ -12,23 +12,31 @@ export const EXPERIENCES = [
     company: "Chegg, India Inc.",
     description:"Solved questions based on multiple programming languages and frameworks for students mainly in the USA",
     technologies: ["Javascript", "MERN stack", "Google Cloud", "C"],
+  },
+
+  {
+    year: "July/2024 - Oct/2024",
+    role: "Backend Development Intern",
+    company: "Webmyze",
+    description:": "Developed and optimized backend services using Node.js, Express.js, and MongoDB, designed RESTful APIs also worked on professional client projects, gaining hands-on experience",
+    technologies: ["MongoDB", " Express.js", "React.js", "Node.js", "API development"],
   }
 ];
 
 export const PROJECTS = [
   {
-    title: "Pomodoro Application Using Next.js",
+    title: "Dexter Luxuries : Online Store for Digital Products",
     image: project1,
     description:
-      "Developed a Pomodoro application using Next.js, leveraging React for the frontend and JavaScript for interactive functionalities. CSS was used for styling, and state management was handled with React hooks. The application included precise timing logic, enhanced with animations using Framer Motion.",
-    technologies: ["JavaScript", "Next.js", "React", "CSS","Framer Motion"],
+      "Built a responsive online Ecom store with React.js frontend and a scalable Node.js + Express backend, handling 1,000+ successful purchases and 150K+ user reach. Integrated MongoDB for secure data storage, Razorpay API for seamless payments, and Meta Pixel + Conversion API for optimized ad performance. Achieved 99% automation from (user footfall -> user purchase )",
+    technologies: ["MongoDB", " Express.js", "React.js", "Node.js", "API development", "Nodemailer", "Render"],
   },
   {
-    title: "(Upcoming) LeloYar - A digital entertainment shopping hub",
+    title: "AI Agent for Current Temperature - Realtime data fetch*",
     image: project2,
     description:
-      "Developed a full-stack e-commerce website using Next.js and React.js, with MongoDB for backend data storage. Leveraged Tailwind CSS and Styled Components for a responsive and visually appealing UI. Implemented key features including user authentication, product management, and secure payment integration.",
-    technologies:["MongoDB","Express","React","Node","Javascript","User Authentication","Tailwind CSS","Next.js"],
+      "Developed an AI-powered weather assistant using Node.js, OpenAI API (GPT-4o), and OpenWeather API to fetch real-time weather data based on user queries.Implemented a conversational AI flow with structured planning, action execution, and observation handling using JavaScript (ES6), Fetch API, and readline-sync for interactive command-line input.",
+    technologies:["React","Node","Javascript,"OpenAI API", "WeatherAPI"],
   },
   
 ];
